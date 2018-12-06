c8b3a069622d8205a41690d52d3da54ae4fc773c
nalbam <me@nalbam.com>
2018-12-06 22:07:04 +0900

    docker ps -a -q -f

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 CLEAN_PERIOD=${CLEAN_PERIOD:-600}
 
-docker ps -a | awk '/Exited|Dead/ {print $1}' | xargs --no-run-if-empty docker rm
+docker ps -a -q -f status=exited -f status=dead | xargs --no-run-if-empty docker rm
 docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
 docker volume ls -q -f dangling=true | xargs --no-run-if-empty docker volume rmi
 
