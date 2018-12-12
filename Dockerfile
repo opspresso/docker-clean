3e0875d53797f4fed09bc54811b342f15e4f6b5e
nalbam <me@nalbam.com>
2018-12-12 15:52:01 +0900

    chmod +x /run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 # Dockerfile
 
-FROM docker
+FROM alpine
+
+RUN apk add --update bash curl docker && \
+    rm -rf /var/cache/apk/*
 
 ADD run.sh /run.sh
+RUN chmod +x /run.sh
 
 ENTRYPOINT ["/run.sh"]
--- run.sh
+++ run.sh
@@ -2,18 +2,20 @@
 
 CLEAN_PERIOD=${CLEAN_PERIOD:-600}
 
-echo "started at $(date)"
+while true; do
+    echo "started at $(date)"
 
-echo "docker ps -a -f status=exited -f status=dead"
-docker ps -a -f status=exited -f status=dead
-docker ps -a -q -f status=exited -f status=dead | xargs --no-run-if-empty docker rm
+    echo "docker ps -a -f status=exited -f status=dead"
+    docker ps -a -f status=exited -f status=dead
+    docker ps -a -q -f status=exited -f status=dead | xargs --no-run-if-empty docker rm
 
-echo "docker images -f dangling=true"
-docker images -f dangling=true
-docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
+    echo "docker images -f dangling=true"
+    docker images -f dangling=true
+    docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
 
-echo "docker volume ls -f dangling=true"
-docker volume ls -f dangling=true
-docker volume ls -q -f dangling=true | xargs --no-run-if-empty docker volume rmi
+    echo "docker volume ls -f dangling=true"
+    docker volume ls -f dangling=true
+    docker volume ls -q -f dangling=true | xargs --no-run-if-empty docker volume rmi
 
-sleep ${CLEAN_PERIOD}
+    sleep ${CLEAN_PERIOD}
+done
