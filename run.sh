357541e7c42c0cdeaa9a6c737926162858e0890c
nalbam <me@nalbam.com>
2018-12-12 15:58:27 +0900

    log

--- run.sh
+++ run.sh
@@ -3,17 +3,21 @@
 CLEAN_PERIOD=${CLEAN_PERIOD:-600}
 
 while true; do
-    echo "started at $(date)"
+    echo
+    echo "# started at $(date)"
 
-    echo "docker ps -a -f status=exited -f status=dead"
+    echo
+    echo "$ docker ps -a -f status=exited -f status=dead"
     docker ps -a -f status=exited -f status=dead
     docker ps -a -q -f status=exited -f status=dead | xargs --no-run-if-empty docker rm
 
-    echo "docker images -f dangling=true"
+    echo
+    echo "$ docker images -f dangling=true"
     docker images -f dangling=true
     docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
 
-    echo "docker volume ls -f dangling=true"
+    echo
+    echo "$ docker volume ls -f dangling=true"
     docker volume ls -f dangling=true
     docker volume ls -q -f dangling=true | xargs --no-run-if-empty docker volume rmi
 
